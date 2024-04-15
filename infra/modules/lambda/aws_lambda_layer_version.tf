ccec8f8bfbe7930343db6e5ad36532fbc41dbdef
sugiyama404 <mmingriha@gmail.com>
2024-04-15 15:32:23 +0900

    delete file

--- infra/modules/lambda/aws_lambda_layer_version.tf
+++
@@ -1,8 +0,0 @@
-# resource "aws_lambda_layer_version" "pymysql" {
-
-#   layer_name = "lambda_layer_name"
-#   filename   = "./modules/lambda/layers/PyMySQL-main.zip"
-
-#   compatible_architectures = ["x86_64"]
-#   compatible_runtimes      = ["python3.9"]
-# }
